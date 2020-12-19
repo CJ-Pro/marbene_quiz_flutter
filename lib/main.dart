af041c470f1be3a91c7f1b6074a40050c66b224d
CJ-Pro <cjn.pro96@gmail.com>
2020-12-19 15:31:19 +0000

    Add auth service

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'package:marbene/view/screens/home/home_screen.dart';
+import 'package:marbene/view/screens/login/login_screen.dart';
+import 'model/services/authentication_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final authService = Get.put(AuthenticationService());
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'Marbene',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: Obx(
+        () => authService.user == null ? LoginScreen() : HomeScreen(),
+      ),
     );
   }
 }
---
+++ lib/model/services/authentication_service.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class AuthenticationService extends GetxService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  Rx<User> _user = Rx<User>();
+
+  @override
+  onInit() {
+    super.onInit();
+    _user.bindStream(_auth.idTokenChanges());
+  }
+
+  User get user => _user.value;
+
+  Future<String> registerWithEmailAndPassword(
+      {String email, String password}) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        return 'The password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        return 'The account already exists for that email.';
+      }
+      return e.message;
+    } catch (e) {
+      print(e);
+      return e.toString();
+    }
+    return null;
+  }
+
+  Future<String> signInWithEmailAndPassword(
+      {String email, String password}) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        return 'No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        return 'Wrong password provided for that user.';
+      }
+
+      return e.message;
+    } catch (e) {
+      print(e);
+      return e.toString();
+    }
+    return null;
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+}
