2c8dac2f6d2e9f3b6354b8604c530daec02d44a9
CJ-Pro <cjn.pro96@gmail.com>
2020-12-22 13:34:15 +0000

    Dynamic home tab

--- lib/view/screens/home/widgets/home_tab.dart
+++ lib/view/screens/home/widgets/home_tab.dart
@@ -2,31 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeTab extends StatelessWidget {
-  const HomeTab({Key key}) : super(key: key);
+  final String title;
+  final VoidCallback onPressed;
+  const HomeTab({Key key, @required this.title, @required this.onPressed})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(
-          vertical: 0.05 * Get.width,
-        ),
-        height: 1 / 7 * Get.height,
-        width: 3 / 5 * Get.width,
-        child: Text(
-          'Create Test',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 22,
+      child: GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            vertical: 0.05 * Get.width,
           ),
-        ),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(
-            0.1 * Get.width,
+          height: 1 / 7 * Get.height,
+          width: 3 / 5 * Get.width,
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 22,
+            ),
+          ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(
+              0.1 * Get.width,
+            ),
           ),
         ),
       ),
