e2200f943fe215a732385562239d0fffc298056c
CJ-Pro <cjn.pro96@gmail.com>
2020-12-28 18:45:11 +0000

    Add assessment implementations

--- lib/model/assessment/multiple_choice_assessment.dart
+++ lib/model/assessment/multiple_choice_assessment.dart
@@ -1,1 +1,75 @@
+import 'dart:convert';
 
+import 'assessment.dart';
+
+class MultipleChoiceAssessment extends Assessment {
+  bool _isCorrect;
+  bool _isFlagged;
+  int _answer;
+  MultipleChoiceAssessment({
+    int id,
+    bool isCorrect,
+    bool isFlagged = false,
+    int answer,
+  })  : _isCorrect = isCorrect,
+        _isFlagged = isFlagged,
+        _answer = answer,
+        super(id);
+  @override
+  bool get correct => _isCorrect;
+
+  @override
+  bool get flagged => _isFlagged;
+
+  @override
+  get solution => _answer;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'isCorrect': _isCorrect,
+      'isFlagged': _isFlagged,
+      'answer': _answer,
+    };
+  }
+
+  factory MultipleChoiceAssessment.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return MultipleChoiceAssessment(
+      id: map['id'],
+      isCorrect: map['isCorrect'],
+      isFlagged: map['isFlagged'],
+      answer: map['answer'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory MultipleChoiceAssessment.fromJson(String source) =>
+      MultipleChoiceAssessment.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'MultipleChoiceAssessment(id: $id, isCorrect: $_isCorrect, isFlagged: $_isFlagged, answer: $_answer)';
+  }
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is MultipleChoiceAssessment &&
+        o.id == id &&
+        o._isCorrect == _isCorrect &&
+        o._isFlagged == _isFlagged &&
+        o._answer == _answer;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        _isCorrect.hashCode ^
+        _isFlagged.hashCode ^
+        _answer.hashCode;
+  }
+}
--- lib/model/assessment/theory_assessment.dart
+++ lib/model/assessment/theory_assessment.dart
@@ -1,1 +1,75 @@
+import 'dart:convert';
 
+import 'assessment.dart';
+
+class TheoryAssessment extends Assessment {
+  bool _isCorrect;
+  bool _isFlagged;
+  List<String> _answers;
+  TheoryAssessment({
+    int id,
+    bool isCorrect,
+    bool isFlagged = false,
+    List<String> answers,
+  })  : _isCorrect = isCorrect,
+        _isFlagged = isFlagged,
+        _answers = answers,
+        super(id);
+  @override
+  bool get correct => _isCorrect;
+
+  @override
+  bool get flagged => _isFlagged;
+
+  @override
+  get solution => _answers;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'isCorrect': _isCorrect,
+      'isFlagged': _isFlagged,
+      'answers': _answers,
+    };
+  }
+
+  factory TheoryAssessment.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return TheoryAssessment(
+      id: map['id'],
+      isCorrect: map['isCorrect'],
+      isFlagged: map['isFlagged'],
+      answers: map['answers'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory TheoryAssessment.fromJson(String source) =>
+      TheoryAssessment.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'MultipleChoiceAssessment(id: $id, isCorrect: $_isCorrect, isFlagged: $_isFlagged, answer: $_answers)';
+  }
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is TheoryAssessment &&
+        o.id == id &&
+        o._isCorrect == _isCorrect &&
+        o._isFlagged == _isFlagged &&
+        o._answers == _answers;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        _isCorrect.hashCode ^
+        _isFlagged.hashCode ^
+        _answers.hashCode;
+  }
+}
