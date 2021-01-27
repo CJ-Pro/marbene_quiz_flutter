b3d07d6c492ab0d809702fbd472aad1444172625
CJ-Pro <cjn.pro96@gmail.com>
2021-01-27 22:32:42 +0000

    Add assessment blueprint widget

--- lib/domain/assessment/widgets/assessment_widget.dart
+++ lib/domain/assessment/widgets/assessment_widget.dart
@@ -4,8 +4,22 @@ import '../../assessment/model/assessment.dart';
 export '../../assessment/model/assessment.dart';
 export 'multiple_choice_assessment_widget/multiple_choice_assessment_widget.dart';
 
-abstract class AssessmentWidget {
+abstract class AssessmentWidget extends StatelessWidget {
   final Assessment assessment;
   AssessmentWidget(this.assessment);
-  Widget render(bool isTutored);
+
+  Widget buildAssessment();
+  Widget bottomBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: buildAssessment(),
+        ),
+        bottomBar(),
+      ],
+    );
+  }
 }
