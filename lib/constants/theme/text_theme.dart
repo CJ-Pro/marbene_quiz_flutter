510b837be8ddce8a07c843425118334ae7ab8b35
CJ-Pro <cjn.pro96@gmail.com>
2021-01-27 22:31:10 +0000

    Add initial text theme

---
+++ lib/constants/theme/text_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const defaultTextTheme = TextTheme(
+  button: TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+  ),
+);
