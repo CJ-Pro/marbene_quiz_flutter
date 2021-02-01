f075d84978aa01fcf2356262555fcc6003754870
CJ-Pro <cjn.pro96@gmail.com>
2021-02-01 13:42:58 +0000

    Add create quiz page

--- lib/domain/quiz/pages/create_quiz/create_quiz_page.dart
+++ lib/domain/quiz/pages/create_quiz/create_quiz_page.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class CreateQuizPage extends StatelessWidget {
+  const CreateQuizPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: ListView(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
