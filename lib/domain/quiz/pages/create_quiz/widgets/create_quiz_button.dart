17b29709fae8497f327b70f601df06f368f14f52
CJ-Pro <cjn.pro96@gmail.com>
2021-02-11 21:48:17 +0000

    Include create quiz function

--- lib/domain/quiz/pages/create_quiz/widgets/create_quiz_button.dart
+++ lib/domain/quiz/pages/create_quiz/widgets/create_quiz_button.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../viewmodel/create_quiz_viewmodel.dart';
 
 class CreateQuizButton extends StatelessWidget {
   const CreateQuizButton({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _viewmodel = Get.find<CreateQuizViewModel>();
     return FlatButton(
       child: Text(
         'CREATE QUIZ',
         style: Theme.of(context).textTheme.button,
       ),
       padding: const EdgeInsets.all(25),
-      onPressed: () {},
+      onPressed: _viewmodel.createQuiz,
       color: Theme.of(context).accentColor,
     );
   }
