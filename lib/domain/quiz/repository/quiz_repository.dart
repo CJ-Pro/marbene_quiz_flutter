97604da02a35885a7426784ee43793e08c8127a8
CJ-Pro <cjn.pro96@gmail.com>
2021-02-11 21:48:25 +0000

    Refactoring

--- lib/domain/quiz/repository/quiz_repository.dart
+++ lib/domain/quiz/repository/quiz_repository.dart
@@ -1,30 +1,33 @@
+import 'dart:collection';
+
 import 'package:get/get.dart';
 
-import '../../../services/authentication_service.dart';
 import '../../../services/database_service.dart';
 import '../../question/model/question.dart';
 import '../model/quiz.dart';
+import '../paths/quiz_paths.dart';
 
 class QuizRepository {
   final _databaseService = Get.find<DatabaseService>();
-  final _userId = Get.find<AuthenticationService>().user.uid;
 
-  String get _userQuizPath => 'flamelink/quiz/$_userId/';
-
-  Future<void> createQuiz(List<Question> questions) async {
-    final assesments = _createAssessments(questions)
-      ..shuffle(); // Shuffle assesments for variability
+  Future<Quiz> createQuiz(Set<Question> questions) async {
+    final assesments = questions
+        .map((question) => question.newAssessment)
+        .toList()
+          ..shuffle(); // Shuffle assesments for variability
     final creationTimeStamp = DateTime.now().millisecondsSinceEpoch;
     final quiz = Quiz(
       id: creationTimeStamp,
-      assessments: assesments,
+      assessments: UnmodifiableListView(assesments),
     );
-    _databaseService.setData(
-        _userQuizPath, creationTimeStamp.toString(), quiz.toMap());
+    _databaseService.setData(FirebaseQuizPath().userQuiz,
+        creationTimeStamp.toString(), quiz.toMap());
+    return Future.value(quiz);
   }
 
   Future<List<Quiz>> getAllPreviousQuiz() async {
-    final snapshot = await _databaseService.getSnapshot(_userQuizPath);
+    final snapshot =
+        await _databaseService.getSnapshot(FirebaseQuizPath().userQuiz);
     return Map<String, dynamic>.from(snapshot.value ?? {})
         ?.values
         ?.map((map) => Quiz.fromMap(Map<String, dynamic>.from(map)))
@@ -32,24 +35,7 @@ class QuizRepository {
   }
 
   void setQuizIndex(int quizId, int index) {
-    _databaseService.updateData('$_userQuizPath/$quizId/', {'index': index});
-  }
-
-  List<Assessment> _createAssessments(List<Question> questions) {
-    return questions?.asMap()?.entries?.map(
-      (entry) {
-        final index = entry.key;
-        final question = entry.value;
-        //TODO Extract switch statement and fx
-        switch (question.category) {
-          case QuestionCategory.multipleChoice:
-            break;
-          case QuestionCategory.pictureTest:
-            break;
-          case QuestionCategory.theory:
-            break;
-        }
-      },
-    )?.toList();
+    _databaseService.updateData(
+        FirebaseQuizPath().getUserQuizById(quizId), {'index': index});
   }
 }
