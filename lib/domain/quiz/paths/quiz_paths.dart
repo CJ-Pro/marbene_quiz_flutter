a2724e5a18054c15d2f54b03943285e8b0a5e2a8
CJ-Pro <cjn.pro96@gmail.com>
2021-02-12 19:54:53 +0000

    Remove narrow quiz firebase path

--- lib/domain/quiz/paths/quiz_paths.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../../../services/authentication_service.dart';
-
-class FirebaseQuizPath {
-  final String _userId = Get.find<AuthenticationService>().user.uid;
-
-  String get userQuiz => 'quiz/$_userId/';
-  String getUserQuizById(int quizId) => 'quiz/$_userId/$quizId/';
-}
