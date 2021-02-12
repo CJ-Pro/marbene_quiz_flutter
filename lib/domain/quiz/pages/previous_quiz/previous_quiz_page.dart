dc57a454d7e83eef5bf4699d47eade552b9dabf6
CJ-Pro <cjn.pro96@gmail.com>
2021-02-12 19:54:36 +0000

    Refactor list item

--- lib/domain/quiz/pages/previous_quiz/previous_quiz_page.dart
+++ lib/domain/quiz/pages/previous_quiz/previous_quiz_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 import '../../../../widgets/loading/loading.dart';
+import '../../model/quiz.dart';
 import '../../viewmodel/previous_quiz_viewmodel.dart';
 import '../start_quiz/start_quiz_page.dart';
 
@@ -10,6 +12,19 @@ class PreviousQuizPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String getYesOrNo(bool value) => value ? 'Yes' : 'No';
+
+    int getPercentageComplete(Quiz quiz) {
+      final assessments = quiz.assessments;
+      final totalAnswered = assessments
+          .map((assessment) => assessment.answer != null ? 1 : 0)
+          .reduce((total, current) => total + current);
+      final percentage = (totalAnswered / assessments.length) * 100;
+      return percentage.round();
+    }
+
+    Widget smallSpacing() => const SizedBox(height: 8);
+
     final _viewmodel = Get.put(PreviousQuizViewModel());
     return Scaffold(
       body: SafeArea(
@@ -25,11 +40,52 @@ class PreviousQuizPage extends StatelessWidget {
                   ),
                 ),
                 child: Card(
+                  elevation: 5,
+                  margin: const EdgeInsets.all(16),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)),
                   child: Padding(
-                    padding: const EdgeInsets.all(50),
-                    child: Text(
-                      'Quiz ${quiz.id}',
-                      textAlign: TextAlign.center,
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Quiz ${index + 1}',
+                          style: Theme.of(context).textTheme.headline1,
+                        ),
+                        smallSpacing(),
+                        Text(
+                          'Created on ${DateFormat.yMMMEd().add_jm().format(DateTime.fromMillisecondsSinceEpoch(quiz.id))}',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        smallSpacing(),
+                        Text(
+                          'Completed: ${getYesOrNo(quiz.isComplete)}',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        smallSpacing(),
+                        Text(
+                          'Tutored: ${getYesOrNo(quiz.isTutored)}',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        smallSpacing(),
+                        Text(
+                          'Timed: ${getYesOrNo(quiz.isTimed)}',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        smallSpacing(),
+                        quiz.isTimed
+                            ? Text(
+                                'Time Left in seconds: ${quiz.timeLeftInSeconds}',
+                                style: Theme.of(context).textTheme.headline2,
+                              )
+                            : const SizedBox.shrink(),
+                        smallSpacing(),
+                        Text(
+                          'Percentage Complete: ${getPercentageComplete(quiz)}%',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                      ],
                     ),
                   ),
                 ),
